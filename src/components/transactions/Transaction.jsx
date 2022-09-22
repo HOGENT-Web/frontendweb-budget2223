2072b82dac5cb4204be33ffd07d37892383b90cf
ksa607 <karine.samyn@hogent.be>
2022-09-22 22:31:12 +0200

    Refactor list of Transactions

--- src/components/transactions/Transaction.jsx
+++ src/components/transactions/Transaction.jsx
@@ -1,6 +1,34 @@
 import { memo } from 'react';
 
-export default memo(function Transaction(props) {
-  const { user, amount, place } = props;
-  return <div className="text-bg-dark" style={{ width: '50%' }}>{user} gaf €{amount} uit bij {place}</div>;
+// kan ook met react-intl (https://formatjs.io/docs/getting-started/installation/)
+const dateFormat = new Intl.DateTimeFormat('nl-BE', {
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric',
+});
+
+const amountFormat = new Intl.NumberFormat('nl-BE', {
+  currency: 'EUR',
+  style: 'currency',
+  maximumFractionDigits: 2,
+  minimumFractionDigits: 2,
+});
+
+export default memo(function Transaction({ user, amount, place, date }) {
+  return (
+    <tr>
+      <td>
+        {dateFormat.format(new Date(date))}
+      </td>
+      <td>{user}</td>
+      <td>{place}</td>
+      <td>
+        {amountFormat.format(amount)}
+      </td>
+      <td>
+        <div className="btn-group float-end">
+        </div>
+      </td>
+    </tr>
+  );
 })
--- src/components/transactions/TransactionList.jsx
+++ src/components/transactions/TransactionList.jsx
@@ -3,6 +3,39 @@ import Transaction from './Transaction';
 import TransactionForm from './TransactionForm';
 import { TRANSACTION_DATA } from '../../api/mock-data';
 
+function TransactionTable({
+  transactions
+}) {
+  if (transactions.length === 0) {
+    return (
+      <div className="alert alert-info">
+        There are no transactions yet.
+      </div>
+    );
+  }
+
+  return (
+    <div className="table-responsive">
+      <table className={`table table-hover`}>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>User</th>
+            <th>Place</th>
+            <th>Amount</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <Transaction key={transaction.id} {...transaction} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
 export default function TransactionList() {
 
   const [transactions, setTransactions] = useState(TRANSACTION_DATA);
@@ -31,7 +64,6 @@ export default function TransactionList() {
         <input type="search" id="search" className="form-control rounded" placeholder="Search" value={text} onChange={(e) => setText(e.target.value)} />
         <button type="button" className="btn btn-outline-primary" onClick={() => setSearch(text)}>Search</button>
       </div>
-      {filteredTransactions.map((trans, index) =>
-        <Transaction {...trans} key={index} />)}
+      <TransactionTable transactions={filteredTransactions} />
     </>);
 }
